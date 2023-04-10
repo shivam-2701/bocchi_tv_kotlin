c2a263ed306bcf9541339f35e84164faca91ea65
shivam-2701 <shivam.shah2701@gmail.com>
2023-04-10 09:38:30 +0530

    UI for search fragment in progress

--- app/src/main/java/com/example/bocchitv/Fragments/SearchFragment.kt
+++ app/src/main/java/com/example/bocchitv/Fragments/SearchFragment.kt
@@ -15,4 +15,9 @@ class SearchFragment :Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_home,container,false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
